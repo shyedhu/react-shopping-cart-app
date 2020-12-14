4353c4fc0d9b98b5ccc156150908404a1d185ede
Shyedhu <shyedhu@users.noreply.github.com>
2020-12-13 19:13:51 -0800

    Update repository.js

--- src/repository.js
+++ src/repository.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'http://localhost:5001';
 
 export function getProducts() {
 	return axios.get(`${BASE_URL}/api/products`)
